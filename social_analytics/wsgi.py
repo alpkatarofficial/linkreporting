65d7efce9355ff934f372fbcf59b2be2eb59bb91
Süleyman Alp Katar <alpkatar@linkdijital.com.tr>
2025-08-07 12:29:35 +0300

    Update wsgi.py

--- social_analytics/wsgi.py
+++ social_analytics/wsgi.py
@@ -1,5 +1,5 @@
-import os
 from django.core.wsgi import get_wsgi_application
+from whitenoise import WhiteNoise
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'social_analytics.settings')
 application = get_wsgi_application()
+application = WhiteNoise(application)
