5b5884378a429a79965572a116bc4c2470a0ce0e
Süleyman Alp Katar <alpkatar@linkdijital.com.tr>
2025-08-07 12:25:53 +0300

    Update settings.py

--- social_analytics/settings.py
+++ social_analytics/settings.py
@@ -1,5 +1,6 @@
 import os
 from pathlib import Path
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -93,3 +94,4 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
